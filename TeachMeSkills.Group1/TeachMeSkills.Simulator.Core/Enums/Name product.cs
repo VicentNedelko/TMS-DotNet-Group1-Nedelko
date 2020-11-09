d611c302b449742697525989efd618abddf909e1
Ваше имя <alesya-13@mail.ru>
2020-11-09 22:48:13 +0300

    feat: add classes

---
+++ TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/Enums/Name product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.Simulator.Core.Enums
+{
+    public enum Name
+    {
+        Product1 = 0,
+        Product2 = 1,
+        Product3 = 2,
+        Product4 = 3,
+        Product5 = 4,
+        Product6 = 5,
+
+    }
+}
---
+++ TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/Seller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.Simulator.Core
+{
+    public class Seller
+    {
+        public int SellerId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Basket> Baskets { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
