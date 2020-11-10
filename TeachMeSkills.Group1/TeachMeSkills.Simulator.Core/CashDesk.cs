5d525f92d065fd0f91f5a6c402b10d5ac639b8aa
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-11-10 22:36:18 +0300

    feat: add Service method body

--- TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/CashDesk.cs
+++ TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/CashDesk.cs
@@ -9,6 +9,8 @@ namespace TeachMeSkills.Simulator.Core
     public class CashDesk
     {
         public Speed Speed { get; set; }
+        public int maxCustomerNumber { get; set; }
+        public Queue<Customer> cashDeskQueue { get; set; }
         private static Semaphore semaphore = new Semaphore(0, 5);
         private int _count;
         public CashDesk()
@@ -29,6 +31,27 @@ namespace TeachMeSkills.Simulator.Core
         }
         public void Service()
         {
+            DateTime startTime;
+            DateTime finishTime;
+            while (maxCustomerNumber > 0 || cashDeskQueue.Count > 0)
+            {
+                if (cashDeskQueue.Count > 0)
+                {
+                    startTime = DateTime.Now;
+                    Thread.Sleep(cashDeskQueue.Peek().Basket.Count * 1000);
+                    finishTime = DateTime.Now;
+                    servingTime.Add(finishTime - startTime);
+                    Console.WriteLine($"Customer served in {(finishTime - startTime).TotalSeconds} sec");
+                    cashDeskQueue.Dequeue();
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("SERVICE FINISHED");
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
     }
