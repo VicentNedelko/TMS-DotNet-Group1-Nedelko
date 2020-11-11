6034ccef624f7e62c578b2e9ff12dd04784d7dc0
Igor Shedko <i.shedko@outlook.com>
2020-11-11 21:29:11 +0300

    add CustomerManager class

---
+++ TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/Managers/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.Simulator.Core.Managers
+{
+    public class CustomerManager
+    {
+        Random RND = new Random();
+        public List<Customer> CustomerGenerator()
+        {
+            var customers = new List<Customer>();
+            for (var i = 0; i < 100; ++i)
+            {
+                var newCustomer = new Customer
+                {
+                    id = i + 1,
+                    GoodsQuantity = RND.Next(1, 10)
+                };
+                customers.Add(newCustomer);
+            }
+            return customers;
+        }
+
+    }
+}
