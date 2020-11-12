2b6b9e61dcc3dff583e5287f9c4716b4aa3ab5a3
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-11-12 21:01:14 +0300

    feat: change CustomerManager Class

--- TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/Managers/CustomerManager.cs
+++ TeachMeSkills.Group1/TeachMeSkills.Simulator.Core/Managers/CustomerManager.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace TeachMeSkills.Simulator.Core.Managers
 {
     public class CustomerManager
     {
-        Random RND = new Random();
-        public List<Customer> CustomerGenerator()
-        {
-            var customers = new List<Customer>();
-            for (var i = 0; i < 100; ++i)
-            {
-                var newCustomer = new Customer
-                {
-                    id = i + 1,
-                    GoodsQuantity = RND.Next(1, 10)
-                };
-                customers.Add(newCustomer);
-            }
-            return customers;
-        }
 
+        public int maxCustomerNumber { get; set; } = 20;
+        Random rand = new Random();
+        public Queue<Customer> commonQueue;
+        public Thread commonQueueThread;
+
+        public CustomerManager()
+        {
+            commonQueue = new Queue<Customer>();
+            commonQueueThread = new Thread(() => CustomerGenerator());
+        }
+        public void CustomerGenerator()
+        {
+            while (maxCustomerNumber > 0)
+            {
+                Thread.Sleep(rand.Next(1000, 3000));
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("New Customer generated.");
+                Console.ForegroundColor = ConsoleColor.White;
+                commonQueue.Enqueue(new Customer());
+                maxCustomerNumber--;
+            }
+        }
     }
 }
